2333c9efa226a5d8f074243046d93f8f11bc21ae
Bart Grzybicki <bgrzybicki@gmail.com>
2014-01-31 23:01:13 +0100

    unicode code fixes

--- plotek.py
+++ plotek.py
@@ -67,18 +67,18 @@ def drawnumbers(gametype, nums_to_draw, number_of_draws):
 
     num_start = 1
     if gametype == 1:
-        game_name = u'Lotto'
+        game_name = 'Lotto'
         num_end = 49
         nums_to_draw = 6
     if gametype == 2:
-        game_name = u'Mini Lotto'
+        game_name = 'Mini Lotto'
         num_end = 42
         nums_to_draw = 5
     if gametype == 3:
-        game_name = u'Multi Multi'
+        game_name = 'Multi Multi'
         num_end = 80
     num_range = xrange(num_start,num_end)
-    nums_all = u''
+    nums_all = ''
     draw = number_of_draws
     for draw in xrange(1, draw+1):
         nums_drawed = random.sample(num_range, nums_to_draw)
@@ -87,25 +87,25 @@ def drawnumbers(gametype, nums_to_draw, number_of_draws):
                 nums_list.append(num)
 
         nums_list.sort()
-        nums_all = u''
+        nums_all = ''
         for item in nums_list:
-                nums_all = nums_all + unicode(item) + u','
+                nums_all = str(nums_all) + str(item) + ','
 
         nums_all = nums_all[:-1]
-        print u'Zakład nr ' + unicode(draw) + u' dla ' + game_name + u': ' + nums_all
+        print 'Zakład nr ' + str(draw) + ' dla ' + game_name + ': ' + str(nums_all)
 
 def main():
 
     clearscreen()
 
-    print u'*********************************'
-    print u'* pLotek 2.0.1 BETA! by bartgee *'
-    print u'*      Python 2 version *'
-    print u'*********************************'
-    print u'1. Lotto'
-    print u'2. Mini Lotto'
-    print u'3. Multi Multi'
-    print u'---------------------------------'
+    print '*********************************'
+    print '* pLotek 2.0.1 BETA! by bartgee *'
+    print '*      Python 2 version *'
+    print '*********************************'
+    print '1. Lotto'
+    print '2. Mini Lotto'
+    print '3. Multi Multi'
+    print '---------------------------------'
 
     # testing code for lottery historical drawings
     try:
@@ -120,57 +120,57 @@ def main():
     running = True
     while running:
         try:
-            gametype = raw_input(u'Wybierz grę LOTTO (1,2,3 lub ENTER dla LOTTO): ')
-            if gametype != u'' and int(gametype) in xrange(1,4):
+            gametype = raw_input('Wybierz grę LOTTO (1,2,3 lub ENTER dla LOTTO): ')
+            if gametype != '' and int(gametype) in xrange(1,4):
                 gametype = int(gametype)
-            if gametype == u'':
+            if gametype == '':
                 gametype = 1
                 nums_to_draw = 6
                 running = False
             elif gametype == 1 or gametype == 2:
-                      nums_to_draw = u''
+                      nums_to_draw = ''
                       running = False
             elif gametype == 3:
                 running = False
                 running2 = True
                 while running2:
                     try:
-                        nums_to_draw = raw_input(u'Podaj ilość typowanych liczb (1-10 lub ENTER dla 10): ')
-                        if nums_to_draw != u'' and int(nums_to_draw) in xrange(1,11):
+                        nums_to_draw = raw_input('Podaj ilość typowanych liczb (1-10 lub ENTER dla 10): ')
+                        if nums_to_draw != '' and int(nums_to_draw) in xrange(1,11):
                             nums_to_draw = int(nums_to_draw)
-                        if nums_to_draw == u'':
+                        if nums_to_draw == '':
                             nums_to_draw = 10
-                            print u'Wybrano 10 liczb.'
+                            print 'Wybrano 10 liczb.'
                             running2 = False
                         elif nums_to_draw in xrange(1,11):
                             running2 = False
                     except:
-                        print u'Wprowadzono błędne dane! 1'
+                        print 'Wprowadzono błędne dane! 1'
                     running = False
         except:
-            print u'Wprowadzono błędne dane! 2'
+            print 'Wprowadzono błędne dane! 2'
     running = True
     while running:
         try:
-            number_of_draws = raw_input(u'Podaj ilość zakładów (od 1 do 20 lub ENTER dla 1): ')
-            if number_of_draws != u'' and int(number_of_draws) in xrange(1,21):
+            number_of_draws = raw_input('Podaj ilość zakładów (od 1 do 20 lub ENTER dla 1): ')
+            if number_of_draws != '' and int(number_of_draws) in xrange(1,21):
                 number_of_draws = int(number_of_draws)
-            if number_of_draws == u'':
+            if number_of_draws == '':
                 number_of_draws = 1
-                print u'Wybrano 1 zakład.'
+                print 'Wybrano 1 zakład.'
                 running = False
             elif number_of_draws in xrange(1, 21):
                 running = False
-            print u'---------------------------------'
+            print '---------------------------------'
         except:
-                print u'Wprowadzono błędne dane!'
+                print 'Wprowadzono błędne dane!'
 
     drawnumbers(gametype, nums_to_draw, number_of_draws)
 
-    restart = raw_input(u'Nowe losowanie? UWAGA - wyniki ostatniego losowania zostaną utracone! (t/n): ')
-    if restart == u't' or restart == u'':
-            if __name__ == u'__main__':
+    restart = raw_input('Nowe losowanie? UWAGA - wyniki ostatniego losowania zostaną utracone! (t/n): ')
+    if restart == 't' or restart == '':
+            if __name__ == '__main__':
                 main()
 
-if __name__ == u'__main__':
+if __name__ == '__main__':
     main()
